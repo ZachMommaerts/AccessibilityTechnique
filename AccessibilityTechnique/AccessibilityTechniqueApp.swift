8a9542e96ba13242a7bd0b1467011725f563066b
ZachMommaerts <zmommaerts@gmail.com>
2023-11-29 12:09:58 -0600

    Initial Commit

---
+++ AccessibilityTechnique/AccessibilityTechniqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTechniqueApp.swift
+//  AccessibilityTechnique
+//
+//  Created by Zach Mommaerts on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTechniqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
